95fce33db33375296357b34bf88cbc9f47164dff
flex <flex.systec@gmail.com>
2025-05-16 14:17:23 -0300

    ajuste

---
+++ app/Console/Commands/ExecutaAusencias.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class ProcessaAfastamentosGeral extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ProcessaAfastamentosGeral';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Executa todos os processos de afastamentos em sequência';
+
+    public function handle()
+    {
+        $key = time();
+        Log::info("{$key} - Início " . static::class);
+
+        $commands = [
+            \App\Console\Commands\InsertAusencia::class,
+            function() {
+                $this->call('nexti-sync', ['folder' => 'absence']);
+            },
+            \App\Console\Commands\ProcessaAusencia::class,
+            \App\Console\Commands\BuscaAusencia::class,
+            \App\Console\Commands\MergeAusencia::class,
+        ];
+
+        foreach ($commands as $command) {
+            if (is_callable($command)) {
+                $command(); 
+            } else {
+                $this->call($command);
+            }
+        }
+
+        Log::info("{$key} - Fim " . static::class);
+
+        return static::SUCCESS;
+    }
+}
