07f60d0fa90809ce64fce54262d8d48d0bd6193f
flex <flex.systec@gmail.com>
2025-05-16 14:19:11 -0300

    ajuste

--- app/Console/Commands/ExecutaAusencias.php
+++ app/Console/Commands/ExecutaAusencias.php
@@ -5,14 +5,14 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
-class ProcessaAfastamentosGeral extends Command
+class ExecutaAusencias extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'ProcessaAfastamentosGeral';
+    protected $signature = 'ExecutaAusencias';
 
     /**
      * The console command description.
